141859532737e61c5a1dea6ad3a8a672863dc648
LBU <lbu@LBUui-MacBook-Pro.local>
2020-12-15 17:00:27 -0500

    initial commit, sketch done

---
+++ api/auth/auth-router.js
@@ -0,0 +1,5 @@
+const bcryptjs = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const router = require('express').Router()
+
+const Users = require('../users/users-model')
---
+++ api/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const helmet = require('helemt')
+const cors = require('cors')
+
+const authRouter = require('./auth/auth-router.js')
+const userRouter = require('./users/users-router')
+
+const server = express()
+server.use(helmet())
+server.use(express.json())
+server.use(cors())
+
+server.use('/api/auth', authRouter)
+server.use('/api/users', userRouter)
+
+server.get('/', (req, res) => {
+    res.json({ api: 'up'})
+})
+
+module.exports = server
